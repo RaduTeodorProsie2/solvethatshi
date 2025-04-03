d5addcc034d6fc47c6e40c4897e7dfc0b47431cd
RaduTeodorProsie2 <abamon.game@gmail.com>
2025-04-03 13:37:55 +0300

    Create the pressing sequence

--- scripts/feed_input.sh
+++ scripts/feed_input.sh
@@ -1,10 +1,20 @@
-sleep 2  
+sleep 2  # Give it time to start
 
+# Send first set of keys with delays
 xdotool key Down
+sleep 0.5
 xdotool key Down
+sleep 0.5
 xdotool key Return
+sleep 0.5
+
+# Send second set of keys with delays
 xdotool key Escape
+sleep 0.5
 xdotool key Down
+sleep 0.5
 xdotool key Down
+sleep 0.5
 xdotool key Down
+sleep 0.5
 xdotool key Return
